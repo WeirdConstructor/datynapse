c431ad9fefd99076bb96b1c6871b09b4a19d8050
Weird Constructor <weirdconstructor@gmail.com>
2020-05-14 22:14:02 +0200

    architect more of the server side

--- src/tcp_csv_msg_connection.rs
+++ src/tcp_csv_msg_connection.rs
@@ -2,6 +2,7 @@ use wlambda::VVal;
 use std::sync::*;
 use std::sync::atomic::AtomicBool;
 use std::net::TcpStream;
+use std::net::TcpListener;
 
 #[derive(Debug, Clone)]
 pub enum Event {
@@ -13,6 +14,72 @@ pub enum Event {
     Quit,
 }
 
+type ClientID = usize;
+
+#[derive(Debug, Clone)]
+pub enum ServerEvent {
+    Client(ClientID, Event),
+    Server(Event),
+}
+
+pub struct TCPCSVServer {
+    op_timeout_ms:      u64,
+    connections:        Vec<Arc<TCPServerConnection>>,
+    free_ids:           Vec<ClientID>,
+}
+
+pub struct TCPServerConInfo {
+    last_activity_time: std::time::SystemTime,
+}
+
+pub struct TCPServerConnection {
+    stream: TcpStream,
+    info:   Mutex<TCPServerConInfo>,
+}
+
+impl TCPCSVServer {
+    fn server_event(&self, e: Event) {
+    }
+
+    fn spawn_server_thread(&mut self) -> Result<(), std::io::Error> {
+        let (stream_tx, stream_rx) = std::sync::mpsc::channel();
+        let l = TcpListener::bind("0.0.0.0:57322")?;
+
+        let op_timeout_ms = self.op_timeout_ms;
+        std::thread::spawn(move || {
+            for stream in l.incoming() {
+                let stream =
+                    match stream {
+                        Ok(s) => s,
+                        Err(e) => {
+                            if let Err(e) =
+                                stream_tx.send(
+                                    Event::ConnectError(
+                                        format!("Client listener error: {}", e)))
+                            {
+                                break;
+                            }
+                            continue;
+                        }
+                    };
+
+                stream.set_nodelay(true)
+                    .expect("Setting TCP no delay");
+                stream.set_read_timeout(
+                    Some(std::time::Duration::from_millis(op_timeout_ms / 10)))
+                    .expect("Setting read timeout");
+                stream.set_write_timeout(
+                    Some(std::time::Duration::from_millis(op_timeout_ms / 10)))
+                    .expect("Setting write timeout");
+                stream.set_nonblocking(true)
+                    .expect("Setting non-blocking");
+
+            }
+        });
+
+        Ok(())
+    }
+}
 
 pub struct TCPCSVConnection {
     op_timeout_ms:      u64,
@@ -20,6 +87,71 @@ pub struct TCPCSVConnection {
     writer_tx:          Option<mpsc::Sender<Option<Vec<u8>>>>,
     pub reader_rx:      Option<mpsc::Receiver<Event>>,
     stop:               std::sync::Arc<AtomicBool>,
+}
+
+fn spawn_writer_thread(mut writer_stream: TcpStream,
+                       wr_ep: String,
+                       writer_event_tx: mpsc::Sender<Event>,
+                       writer_rx: std::sync::Arc<std::sync::Mutex<std::sync::mpsc::Receiver<Option<Vec<u8>>>>>)
+    -> std::thread::JoinHandle<()>
+{
+    std::thread::spawn(move || {
+        use std::io::Write;
+        match writer_rx.lock() {
+            Ok(wr_rx) => {
+                while let Ok(Some(bytes)) = wr_rx.recv() {
+                    if let Err(e) = writer_stream.write_all(&bytes) {
+                        match e.kind() {
+                            std::io::ErrorKind::TimedOut
+                            | std::io::ErrorKind::WouldBlock => {
+                                if let Err(e) = writer_event_tx.send(
+                                    Event::DroppedMessage) {
+                                }
+                                continue;
+                            },
+                            _ => {
+                                if let Err(e) = writer_event_tx.send(
+                                    Event::DroppedMessage) {
+                                }
+
+                                if let Err(e) = writer_event_tx.send(
+                                    Event::ConnectError(
+                                        format!("Write error from {}: {}",
+                                                wr_ep, e))) {
+                                }
+                                break;
+                            }
+                        }
+                    }
+
+                    if let Err(e) = writer_stream.flush() {
+                        if let Err(e) = writer_event_tx.send(
+                            Event::DroppedMessage) {
+                        }
+
+                        if let Err(_) = writer_event_tx.send(
+                            Event::ConnectError(
+                                format!("Write flush error from {}: {}",
+                                        wr_ep, e))) {
+                        };
+                        break;
+                    }
+                    if let Err(e) = writer_event_tx.send(
+                        Event::FlushedMessage) {
+                    }
+                }
+            },
+            Err(e) => {
+                if let Err(_) = writer_event_tx.send(
+                    Event::ConnectError(
+                        format!("Write queue mutex error from {}: {}",
+                                wr_ep, e))) {
+                };
+            },
+        }
+
+        //d// eprintln!("writer ends");
+    })
 }
 
 impl TCPCSVConnection {
@@ -97,63 +229,9 @@ impl TCPCSVConnection {
                             stream
                             .try_clone()
                             .expect("Cloning stream should work for writer!");
-                        let writer = std::thread::spawn(move || {
-                            use std::io::Write;
-                            match writer_rx.lock() {
-                                Ok(wr_rx) => {
-                                    while let Ok(Some(bytes)) = wr_rx.recv() {
-                                        if let Err(e) = writer_stream.write_all(&bytes) {
-                                            match e.kind() {
-                                                std::io::ErrorKind::TimedOut
-                                                | std::io::ErrorKind::WouldBlock => {
-                                                    if let Err(e) = writer_event_tx.send(
-                                                        Event::DroppedMessage) {
-                                                    }
-                                                    continue;
-                                                },
-                                                _ => {
-                                                    if let Err(e) = writer_event_tx.send(
-                                                        Event::DroppedMessage) {
-                                                    }
-
-                                                    if let Err(e) = writer_event_tx.send(
-                                                        Event::ConnectError(
-                                                            format!("Write error from {}: {}",
-                                                                    wr_ep, e))) {
-                                                    }
-                                                    break;
-                                                }
-                                            }
-                                        }
-
-                                        if let Err(e) = writer_stream.flush() {
-                                            if let Err(e) = writer_event_tx.send(
-                                                Event::DroppedMessage) {
-                                            }
-
-                                            if let Err(_) = writer_event_tx.send(
-                                                Event::ConnectError(
-                                                    format!("Write flush error from {}: {}",
-                                                            wr_ep, e))) {
-                                            };
-                                            break;
-                                        }
-                                        if let Err(e) = writer_event_tx.send(
-                                            Event::FlushedMessage) {
-                                        }
-                                    }
-                                },
-                                Err(e) => {
-                                    if let Err(_) = writer_event_tx.send(
-                                        Event::ConnectError(
-                                            format!("Write queue mutex error from {}: {}",
-                                                    wr_ep, e))) {
-                                    };
-                                },
-                            }
-
-                            //d// eprintln!("writer ends");
-                        });
+                        let writer =
+                            spawn_writer_thread(
+                                writer_stream, wr_ep, writer_event_tx, writer_rx);
 
                         if let Err(e) =
                             event_tx.send(
