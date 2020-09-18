6f05dd6f9cef8b78bdd0f12b9adeedaf553739ed
Weird Constructor <weirdconstructor@gmail.com>
2020-09-18 22:16:06 +0200

    more ideas

--- src/tcp_point.rs
+++ src/tcp_point.rs
@@ -1,6 +1,6 @@
 #[derive(Debug, PartialEq, Copy, Clone)]
 struct TCPId {
-    SrvClient(usize),
+    SrvClient(usize, usize),
     Connection(usize),
 }
 
@@ -11,46 +11,45 @@ let tcppoint = TCPPoint::new();
 let id_srv = tcppoint.listen_on(30232);
 let id_srv2 = tcppoint.listen_on(302);
 
-let id = tcppoint.connect_to("127.0.0.1", 2131);
-let id2 = tcppoint.connect_to("192.168.3.3", 2131);
+let id = tcppoint.connect_to("127.0.0.1", 2131, TimeoutMS(100));
+let id2 = tcppoint.connect_to("192.168.3.3", 2131, TimeoutMS(100));
 
 tcppoint.send(id, "hello\r\n");
 
-while let Some(msg) = tcppoint.next() {
+while let Some((id, msg)) = tcppoint.next() {
+    if id.is_srv() {
+        ...
+    }
+
     match msg {
-        TCPSrv(id, msg) => {
-            match msg {
-                TCPConnected(id) => {
-                    let epinfo = tcppoint.get_endpoint_info(id);
-                    tcppoint.send(id, "hello\r\n");
-                },
-                TCPDisconnected(id) => {
-                },
-                TCPError(id, err) => {
-                },
-                TCPData(id, data) => {
-                    if data.contains(x) {
-                        tcppoint.send("quit\r\n");
-                        tcppoint.disconnect_after_send(id);
-                        tcppoint.disconnect_now(id);
-                    }
-                },
-            }
+        TCPSendQueueEmpty() => {
         },
-        TCPConnected(id) => {
+        TCPConnected() => {
             let epinfo = tcppoint.get_endpoint_info(id);
         },
-        TCPDisconnected(id) => {
+        TCPDisconnected() => {
         },
-        TCPError(id, err) => {
+        TCPError(err) => {
         },
-        TCPData(id, data) => {
+        TCPData(data) => {
+            if data.contains(x) {
+                tcppoint.send("quit\r\n");
+                tcppoint.disconnect_after_send(id);
+                tcppoint.disconnect_now(id);
+            }
         },
     }
 }
 
+let id = tcppoint.connect_to(...., Timeout(100));
+let sync_con = tcppoint.sync_handle(id);
 
+sync_con.send("foo"); // Waits until error, disconnect or queue empty
 
+sync_con.read_some(); // waits until data, disconnect or queue empty
 
+sync_con.wait_disconnect(); // waits until disconnect or timeout
+
+sync_con.wait_connect();    // waits until connect or timeout
 
 */
